c4b7e1cf528434d13ff51c5d51c263183da8214e
José Luis Alavez Manzano <alavezman@gmail.com>
2025-09-03 00:18:16 -0600

    Added base code for the jascript refresher

---
+++ the-complete-guide/javascrip-refresher/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,7 @@
+///import * as util from "./util.js";
+
+//console.log(util.apiKey + " fff");
+//console.log(util.abc + " fff");
+//console.log(util.def + " fff");
+
+console.log("Importando todo el modulo");
---
+++ the-complete-guide/javascrip-refresher/00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,4 @@
+export default "Esto es una API key";
+export let abc = "Esto es una variable abc";
+export let def = "Esto es una variable def";
+
